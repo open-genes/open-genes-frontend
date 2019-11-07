a755f5881fe484d65ab0acfacda55a24fc28542b
Isoboy <isoboy.k@gmail.com>
2019-11-07 15:44:35 +0500

    sort genes by ageMya

--- src/app/shared/table/table.component.ts
+++ src/app/shared/table/table.component.ts
@@ -1,16 +1,19 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import { IGen } from '../../core/models';
+import {fromEvent, Subject} from 'rxjs';
+import {takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnDestroy {
 
   @Input() dataSource: IGen[];
   activeGenesQuantity = 20;
   isSorted;
+  private subscription$ = new Subject();
 
   constructor() { }
 
@@ -19,14 +22,14 @@ export class TableComponent implements OnInit {
   }
 
   getGenes() {
-    this.isSorted ? this.reverse() : this.sort();
+    this.isSorted ? this.reverse() : this.sortByName();
     this.isSorted = !this.isSorted;
   }
 
   private reverse() {
     this.dataSource.reverse();
   }
-  private sort() {
+  private sortByName() {
     this.dataSource.sort((a, b) => {
       const A = (a.symbol + a.name).toLowerCase();
       const B = (b.symbol + b.name).toLowerCase();
@@ -38,14 +41,30 @@ export class TableComponent implements OnInit {
       return 0;
     });
   }
-  private getScrollPosition() {
-    window.onscroll = () => {
-      const d = document.documentElement;
-      const offset = d.scrollTop + window.innerHeight;
-      const height = d.offsetHeight;
-      if (offset >= height - 1) {
-        this.activeGenesQuantity += 20;
+  private sortByAge() {
+    this.dataSource.sort((a, b) => {
+      const A = a.ageMya;
+      const B = b.ageMya;
+      if (A - B) {
+        return -1;
       }
-    };
+      return 0;
+    });
+  }
+  private getScrollPosition() {
+    fromEvent(document, 'scroll')
+      .pipe(takeUntil(this.subscription$))
+      .subscribe(() => {
+        const d = document.documentElement;
+        const offset = d.scrollTop + window.innerHeight;
+        const height = d.offsetHeight;
+        if (offset >= height - 20 && this.dataSource.length >= this.activeGenesQuantity) {
+          this.activeGenesQuantity += 20;
+        }
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
   }
 }
