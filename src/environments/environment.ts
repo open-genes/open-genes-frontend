4a628ff382b6a2ad0d534895cc0669c5230833f9
Constantine Rafikov <kon.rafikov@yandex.ru>
2019-11-30 18:35:14 +0300

    Kludge fix for prod build, new job for a pipe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://dev.open-genes.com',
+  production: true,
+  apiUrl: 'http://open-genes.com',
   languages: ['ru', 'en']
 };
 
