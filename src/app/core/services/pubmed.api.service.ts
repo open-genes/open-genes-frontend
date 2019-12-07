62ac0ceddda18be511758882e65b7c60c20a0bb8
Constantine Rafikov <kon.rafikov@yandex.ru>
2019-12-07 17:29:24 +0300

    Pubmed query filter

--- src/app/core/services/pubmed.api.service.ts
+++ src/app/core/services/pubmed.api.service.ts
@@ -9,14 +9,15 @@ import {environment} from '../../../environments/environment';
 export class PubmedApiService {
 
   private url = environment.pubmedApiUrl;
-  private limit = 5;
+  private limit = 3;
   private params = 'db=pubmed&retmode=json';
 
   constructor(private http: HttpClient) {
   }
 
   public getNewsList(symbol: string): Observable<any> {
-    return this.http.get(`${this.url}/esearch.fcgi?${this.params}&retmax=${this.limit}&term=${symbol}[Title/Abstract]`);
+    // tslint:disable-next-line:max-line-length
+    return this.http.get(`${this.url}/esearch.fcgi?${this.params}&retmax=${this.limit}&term=${symbol}[Title]&&2007:2019[pubdate]`);
   }
 
   public getNewsData(id: number): Observable<any> {
