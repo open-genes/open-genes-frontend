7223f141d21a80cb80ad18d4171d0e686a338f18
bdf2ch <bdf2ch@gmail.com>
2019-12-17 11:50:57 +0300

    News list mixed with articles of different genes for last 6 months, loading spinner added

--- src/app/shared/news/news.component.ts
+++ src/app/shared/news/news.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {PubmedApiService} from '../../core/services/pubmed.api.service';
 import {INews} from '../../core/models/news.model';
 import {IGene} from '../../core/models';
-import {switchMap} from 'rxjs/operators';
+import {finalize, switchMap} from 'rxjs/operators';
 import {environment} from '../../../environments/environment';
 
 @Component({
@@ -12,10 +12,13 @@ import {environment} from '../../../environments/environment';
 })
 export class NewsComponent implements OnInit {
 
-  @Input() gene: IGene;
-  newsList: INews[] = [];
+  @Input() genes: IGene[];
+  newsList: INews[];
+  loading: boolean;
 
   constructor(private pubmedApiService: PubmedApiService) {
+    this.newsList = [];
+    this.loading = true;
   }
 
   ngOnInit() {
@@ -23,15 +26,33 @@ export class NewsComponent implements OnInit {
   }
 
   private getNews() {
-    this.pubmedApiService.getNewsList(this.gene.symbol).pipe(
+    let symbolsQuery = '';
+    const filteredGenes = this.genes.filter((gene: IGene) => gene.functionalClusters.length > 2 ? true : false);
+    filteredGenes.forEach((gene: IGene, index: number, array: IGene[]) => {
+      symbolsQuery += `${gene.symbol}[Title]`;
+      symbolsQuery += index < array.length - 1 ? '+OR+' : '';
+    });
+    this.pubmedApiService.getNewsList(symbolsQuery).pipe(
       switchMap(news => {
-        return this.pubmedApiService.getNewsData(news.esearchresult.idlist);
+        return this.pubmedApiService.getNewsData(
+          news.esearchresult.idlist
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 3)
+        );
+      }),
+      finalize(() => {
+        this.loading = false;
       })
     ).subscribe(data => {
       data.result.uids.forEach(id => {
-        this.newsList.push({
-          url: environment.pubmedUrl + id,
-          title: data.result[id].title
+        filteredGenes.forEach((gene: IGene) => {
+          if (data.result[id].title.toLowerCase().indexOf(gene.symbol.toLowerCase()) !== -1) {
+            this.newsList.push({
+              url: environment.pubmedUrl + id,
+              title: data.result[id].title,
+              gene
+            });
+          }
         });
       });
     });
