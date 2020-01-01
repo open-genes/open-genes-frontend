ce997b769948213c052bfe2fdd45db3704434678
Constantine Rafikov <kon.rafikov@yandex.ru>
2020-01-01 20:30:31 +0300

    API page, component for code snippets, some ui improvements

--- src/app/modules/api/api.module.ts
+++ src/app/modules/api/api.module.ts
@@ -3,17 +3,20 @@ import {CommonModule} from '@angular/common';
 import {ApiComponent} from './api.component';
 import {RouterModule, Routes} from '@angular/router';
 import {TranslateModule} from '@ngx-translate/core';
+import {CodeComponent} from '../../common/code/code.component';
+import {PipesModule} from "../home/pipes/pipes.module";
 
 const routes: Routes = [
   {path: '', component: ApiComponent}
 ];
 
 @NgModule({
-  declarations: [ApiComponent],
+  declarations: [ApiComponent, CodeComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-    TranslateModule
+    TranslateModule,
+    PipesModule
   ]
 })
 export class ApiModule {
