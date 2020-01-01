a9fdb361751bb975db42dbd1c81c8edc27fc506a
Constantine Rafikov <kon.rafikov@yandex.ru>
2020-01-02 00:17:07 +0300

    hotfix

--- src/app/common/burger-menu/burger-menu.component.ts
+++ src/app/common/burger-menu/burger-menu.component.ts
@@ -2,8 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-burger-menu',
-  templateUrl: './app-burger-menu.component.html',
-  styleUrls: ['./app-burger-menu.component.scss']
+  templateUrl: './burger-menu.component.html',
+  styleUrls: ['./burger-menu.component.scss']
 })
 export class BurgerMenuComponent {
 
