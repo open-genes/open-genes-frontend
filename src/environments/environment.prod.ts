a25f3f12f9a12c91d14afa3a903fdbfff2376c67
konstantinrafikov <k.rafikov@gateline.ru>
2020-04-08 21:59:28 +0300

    Added https url for prod environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://open-genes.com',
+  apiUrl: 'https://open-genes.com',
   pubmedApiUrl: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils',
   pubmedUrl: 'https://www.ncbi.nlm.nih.gov/pubmed/',
   debugMode: false,
