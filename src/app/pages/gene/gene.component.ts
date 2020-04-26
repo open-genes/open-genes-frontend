74517daa0d0bc2fd9f8f213f528eb4c0a4daa844
konstantinrafikov <k.rafikov@gateline.ru>
2020-04-26 16:12:43 +0300

    Added orthologs output on a gene page

--- src/app/pages/gene/gene.component.ts
+++ src/app/pages/gene/gene.component.ts
@@ -16,12 +16,13 @@ export class GeneComponent implements OnInit, OnDestroy {
               private apiService: ApiService) {
     this.subscription = activateRoute.params.subscribe(params => this.symbol = params.id);
   }
+
   public symbol: string;
   private subscription: Subscription;
   public gene: any;
-  public geneOntologyProcess: Map<string, string>;
-  public geneOntologyComponent: Map<string, string>;
-  public geneOntologyActivity: Map<string, string>;
+  public geneOntologyProcessMap: Map<string, string>;
+  public geneOntologyComponentMap: Map<string, string>;
+  public geneOntologyActivityMap: Map<string, string>;
 
   static toMap(object) {
     const mappedObj = new Map();
@@ -31,20 +32,19 @@ export class GeneComponent implements OnInit, OnDestroy {
       }
     }
     return mappedObj;
-    // console.log(mappedObj);
   }
 
   ngOnInit() {
     this.getGene();
-    // console.log(this.geneOntologyProcess);
   }
 
   private getGene() {
     this.apiService.getGeneByHGNCsymbol(this.symbol).subscribe((geneInterface) => {
       this.gene = geneInterface;
-      this.geneOntologyProcess = GeneComponent.toMap(this.gene.terms.biological_process);
-      this.geneOntologyComponent = GeneComponent.toMap(this.gene.terms.cellular_component);
-      this.geneOntologyActivity = GeneComponent.toMap(this.gene.terms.molecular_activity);
+      this.geneOntologyProcessMap = GeneComponent.toMap(this.gene.terms.biological_process);
+      this.geneOntologyComponentMap = GeneComponent.toMap(this.gene.terms.cellular_component);
+      this.geneOntologyActivityMap = GeneComponent.toMap(this.gene.terms.molecular_activity);
+      console.log(this.gene.orthologs);
     });
   }
 
@@ -61,6 +61,7 @@ export class GeneComponent implements OnInit, OnDestroy {
       this.gene.researches.geneAssociatedWithProgeriaSyndromes.length !== 0 ||
       this.gene.researches.geneAssociatedWithLongevityEffects.length !== 0 ||
       this.gene.expression.length !== 0 ||
+      this.gene.orthologs.length !== 0 ||
       this.gene.terms);
   }
 
