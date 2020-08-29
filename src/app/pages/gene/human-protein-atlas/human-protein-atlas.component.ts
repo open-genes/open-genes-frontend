aebbb7183acd6991e9075e63eb0f0023232dd11d
Constantine Rafikov <kon.rafikov@yandex.ru>
2020-08-30 00:10:02 +0300

    Gene page. HPA: check for objects with the same data in response. Some styles.

--- src/app/pages/gene/human-protein-atlas/human-protein-atlas.component.ts
+++ src/app/pages/gene/human-protein-atlas/human-protein-atlas.component.ts
@@ -8,10 +8,17 @@ import { TranslateService } from '@ngx-translate/core';
 
 export class HumanProteinAtlasComponent implements OnInit {
   @Input() public gene: any;
+  @Input() public hpa: any; // shorthand property
 
   constructor(public translate: TranslateService) { }
 
   ngOnInit() {
+  }
 
+  /**
+   * Checks if two objects are similar
+   */
+  public isSimilar(object1: object, object2: object) {
+    return JSON.stringify(object1) === JSON.stringify(object2); // boolean
   }
 }
