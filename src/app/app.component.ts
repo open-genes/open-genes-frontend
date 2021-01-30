620b1cde2e87095eb18a67787096e5d712ebcfbb
Constantine <kon.rafikov@yandex.ru>
2021-01-30 19:19:34 +0300

    Minor fix for AppComponent: DI, removed OnInit implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,24 @@
-import { Component, AfterViewChecked } from "@angular/core";
+import { Component, AfterViewChecked, Inject } from "@angular/core";
 import { TranslateService } from "@ngx-translate/core";
 import { environment } from "../environments/environment";
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent implements OnInit, AfterViewChecked {
+export class AppComponent implements AfterViewChecked {
   private lang: string;
 
-  constructor(private translate: TranslateService) {
+  constructor(
+    private translate: TranslateService,
+    @Inject(DOCUMENT) public document: Document,
+  ) {
     this.translate.addLangs(environment.languages);
     if (localStorage.getItem('lang')) {
       this.lang = localStorage.getItem('lang')
     } else if (navigator.language.substring(0, 2) === 'en' || navigator.language.substring(0, 2) === 'ru') {
-       this.lang = navigator.language.substring(0, 2)
+      this.lang = navigator.language.substring(0, 2)
     } else {
       this.lang = environment.languages[0];
     }
@@ -24,7 +28,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
   public ngAfterViewChecked(): void {
     setTimeout(() => {
       // TODO: find a proper way to wait for the full DOM loading considering all modules
-      document.body.classList.remove("body--loading");
+      this.document.body.classList.remove("body--loading");
     }, 500);
   }
 }
