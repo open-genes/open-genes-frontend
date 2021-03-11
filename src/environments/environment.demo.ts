7955b9bfe784a033dfeda90f886d4bbe52df9e13
Constantine <kon.rafikov@yandex.ru>
2021-03-11 22:44:43 +0300

    Changes in environments, tasks and CI

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://staging.open-genes.com",
+  apiUrl: "https://demo.open-genes.com",
   pubmedApiUrl: "https://eutils.ncbi.nlm.nih.gov/entrez/eutils",
   pubmedUrl: "https://www.ncbi.nlm.nih.gov/pubmed/",
   proteinAtlasUrl: "https://www.proteinatlas.org/",
