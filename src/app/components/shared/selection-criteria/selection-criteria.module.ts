c4f4654468446636316dc11b536ecbf99678f968
Constantine <kon.rafikov@yandex.ru>
2021-04-18 14:09:40 +0300

    #37. Criteria list error

--- src/app/components/shared/selection-criteria/selection-criteria.module.ts
+++ src/app/components/shared/selection-criteria/selection-criteria.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { SelectionCriteriaComponent } from "./selection-criteria.component";
 import { UiComponentsModule } from "../../ui-components/ui-components.module";
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [SelectionCriteriaComponent],
-  imports: [CommonModule, UiComponentsModule],
+  imports: [CommonModule, UiComponentsModule, TranslateModule],
   exports: [SelectionCriteriaComponent],
 })
 export class SelectionCriteriaModule {}
