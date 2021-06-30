f683d699ece277ad884234b599acd9151db1084f
Constantine <kon.rafikov@yandex.ru>
2021-06-30 23:15:30 +0300

    Language switcher is moved back to the header. Fixed Prettier config.

--- src/app/components/language/language.component.ts
+++ src/app/components/language/language.component.ts
@@ -1,16 +1,20 @@
-import { Component } from "@angular/core";
-import { TranslateService } from "@ngx-translate/core";
-import { environment } from "../../../environments/environment";
+import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
-  selector: "app-language",
-  templateUrl: "./language.component.html",
-  styleUrls: ["./language.component.scss"],
+  selector: 'app-language',
+  templateUrl: './language.component.html',
+  styleUrls: ['./language.component.scss'],
 })
 export class LanguageComponent {
-  languages = environment.languages;
-  currentIndex = environment.languages.indexOf(this.translate.currentLang);
-  nextIndex = (this.currentIndex + 1) % environment.languages.length;
+  @Input() theme: 'light' | 'dark' = 'dark';
+
+  public languages = environment.languages;
+  public currentIndex = environment.languages.indexOf(
+    this.translate.currentLang
+  );
+  public nextIndex = (this.currentIndex + 1) % environment.languages.length;
 
   constructor(public translate: TranslateService) {}
 
@@ -21,9 +25,9 @@ export class LanguageComponent {
     const nextIndex = (currentIndex + 1) % environment.languages.length;
     const nextLang = environment.languages[nextIndex];
     const languageSelector = (l: string) => {
-      if (l === "ru") {
+      if (l === 'ru') {
         return environment.languages[0];
-      } else if (l === "en") {
+      } else if (l === 'en') {
         return environment.languages[1];
       } else {
         return nextLang;
@@ -33,7 +37,7 @@ export class LanguageComponent {
     const languageSet = languageSelector(language);
 
     this.translate.use(languageSet);
-    localStorage.setItem("lang", languageSet);
+    localStorage.setItem('lang', languageSet);
     window.location.reload();
   }
 }
