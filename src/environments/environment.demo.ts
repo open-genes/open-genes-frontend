4ec076a70cb87b9c8805266b0cc05c3b6f5f55a7
Constantine <kon.rafikov@yandex.ru>
2021-07-15 16:43:42 +0300

    OG-104. Changed environment variable

--- src/environments/environment.demo.ts
+++ src/environments/environment.demo.ts
@@ -4,9 +4,9 @@ export const environment = {
   pubmedApiUrl: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils',
   pubmedUrl: 'https://www.ncbi.nlm.nih.gov/pubmed/',
   proteinAtlasUrl: 'https://www.proteinatlas.org/',
-  openLongevity80LevelCMS: 'https://everything.openlongevity.org',
+  openLongevity80LevelCMS: 'https://openlongevity.tk',
   debugMode: false,
   languages: ['ru', 'en'],
   mockJsonUrl: 'assets/data/api.mock.json',
   gaTrackingCode: 'G-825YYNSJFC',
-}
+};
