31ea540f9e251dc2a94ee9ee8301eaed0fbfdab3
Constantine <kon.rafikov@yandex.ru>
2021-07-27 01:30:03 +0300

    OG-191. WIP: articles in a sidebar. Sidebar component.

---
+++ src/app/components/shared/sidebar/sidebar-item/sidebar-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-item',
+  templateUrl: './sidebar-item.component.html',
+  styleUrls: ['./sidebar-item.component.scss'],
+})
+export class SidebarItemComponent {}
