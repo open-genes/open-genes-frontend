447637009dfc751a201968c0165cfea5073565eb
Constantine <kon.rafikov@yandex.ru>
2021-08-12 17:41:32 +0300

    OG-242. EightyLevelService unit test

---
+++ src/app/core/services/api/80level-api-service/80level-api.service.spec.ts
@@ -0,0 +1,23 @@
+import { EightyLevelService } from './80level-api.service';
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { I80levelResponseAllArticles } from '../../../models/vendorsApi/80level/80level.model';
+
+describe('EightyLevelService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+  });
+
+  it('getArticles method should return correct data structure', () => {
+    const service = TestBed.get(EightyLevelService);
+    service
+      .getArticles(null)
+      .subscribe(async (response: I80levelResponseAllArticles) => {
+        const articles = response.articles;
+        expect(articles).toContain('total');
+        expect(articles).toContain('items');
+      });
+  });
+});
