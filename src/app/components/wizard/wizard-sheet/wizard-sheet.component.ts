6d6d9f66d87ac771607a0e4550e74333a57c6c1b
Constantine <kon.rafikov@yandex.ru>
2021-08-23 01:10:29 +0300

    OG-247. Wizard sheet - added title, vertical stepper

--- src/app/components/wizard/wizard-sheet/wizard-sheet.component.ts
+++ src/app/components/wizard/wizard-sheet/wizard-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatStepper } from '@angular/material/stepper';
 import { WizardService } from '../wizard-service.service';
 
