1389e6e241b2a3afe9a054c8346b93a9ca9a7793
Constantine <kon.rafikov@yandex.ru>
2021-08-29 09:57:04 +0300

    OG-247. Swipe directive for wizard bottom sheet

--- src/app/directives/directives.module.ts
+++ src/app/directives/directives.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { HighlightDirective } from "./highlight.directive";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HighlightDirective } from './highlight.directive';
+import { SwipeDirective } from './swipe.directive';
 
-const DIRECTIVES = [HighlightDirective];
+const DIRECTIVES = [HighlightDirective, SwipeDirective];
 
 @NgModule({
   declarations: [...DIRECTIVES],
