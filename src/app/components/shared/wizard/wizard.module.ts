7d84e51b57690435abb8c23f11c820f592131114
Constantine <kon.rafikov@yandex.ru>
2021-08-29 12:07:52 +0300

    OG-247. Close bottom sheet with swipe

--- src/app/components/shared/wizard/wizard.module.ts
+++ src/app/components/shared/wizard/wizard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { WizardSheetComponent } from './wizard-sheet/wizard-sheet.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatStepperModule } from '@angular/material/stepper';
+import { DirectivesModule } from '../../../directives/directives.module';
 
 @NgModule({
   declarations: [WizardSheetComponent],
-  imports: [CommonModule, TranslateModule, MatStepperModule],
+  imports: [CommonModule, TranslateModule, MatStepperModule, DirectivesModule],
 })
 export class WizardModule {}
