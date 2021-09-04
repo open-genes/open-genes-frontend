9249290d1559d5eb91e2743dcc8b9d343cf814cd
Constantine <kon.rafikov@yandex.ru>
2021-09-04 10:45:53 +0300

    Noissue. Fix bottom sheet issue on swipes and close button

--- src/app/core/utils/window-width.ts
+++ src/app/core/utils/window-width.ts
@@ -5,11 +5,16 @@ import { Subject } from 'rxjs';
 export abstract class WindowWidth {
   public subscription$ = new Subject();
   public isMobile: boolean;
+  public isTouchDevice = false;
   public breakpoints = {
     desktop: 1199.98,
   };
 
-  constructor(public windowService: WindowService) {}
+  constructor(public windowService: WindowService) {
+    if ('ontouchstart' in document.documentElement) {
+      this.isTouchDevice = true;
+    }
+  }
 
   protected initWindowWidth(callback: any): void {
     this.windowService
