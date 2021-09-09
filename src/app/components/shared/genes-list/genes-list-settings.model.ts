9154eca65cc1951316bebddf2c445c4a0d18acae
Constantine Rafikov <kon.rafikov@yandex.ru>
2021-09-10 00:15:16 +0400

    OG-243. Associated diseases. Accordion styles. Filters, translations

--- src/app/components/shared/genes-list/genes-list-settings.model.ts
+++ src/app/components/shared/genes-list/genes-list-settings.model.ts
@@ -2,6 +2,7 @@ export interface GenesListSettings {
   ifShowAge: boolean;
   ifShowClasses: boolean;
   ifShowDiseases: boolean;
+  ifShowDiseaseCategories: boolean;
   ifShowExpression: boolean;
   ifShowCriteria: boolean;
   ifShowMethylation: boolean;
--- src/app/components/shared/genes-list/genes-list.module.ts
+++ src/app/components/shared/genes-list/genes-list.module.ts
@@ -20,14 +20,10 @@ import { SelectionCriteriaModule } from "../selection-criteria/selection-criteri
 import { MaterialModule } from '../../../modules/vendors/material.module';
 import { GenesTableHeaderComponent } from './components/genes-table-header/genes-table-header.component';
 import { AssociatedDiseasesModule } from '../associated-diseases/associated-diseases.module';
+import { AssociatedDiseaseCategoriesModule } from '../associated-disease-categories/associated-disease-categories.module';
 
 @NgModule({
-  declarations: [
-    GenesListComponent,
-    SearchComponent,
-    GeneMenuComponent,
-    GenesTableHeaderComponent,
-  ],
+  declarations: [GenesListComponent, SearchComponent, GeneMenuComponent, GenesTableHeaderComponent],
   imports: [
     CommonModule,
     ReactiveFormsModule,
@@ -45,6 +41,7 @@ import { AssociatedDiseasesModule } from '../associated-diseases/associated-dise
     SelectionCriteriaModule,
     MaterialModule,
     AssociatedDiseasesModule,
+    AssociatedDiseaseCategoriesModule,
   ],
   providers: [WindowService],
   exports: [GenesListComponent, SearchComponent],
