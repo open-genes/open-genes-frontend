b49e7d737d4ebdecb353f461cae171f57965eda5
Said <nanocoding.said@gmail.com>
2021-09-17 10:33:12 +0500

    OG-291.Made some correction

--- src/app/pages/diagrams/diagrams.module.ts
+++ src/app/pages/diagrams/diagrams.module.ts
@@ -4,13 +4,11 @@ import { DiagramsComponent } from './diagrams.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DirectedGraphComponent } from './charts/directed-graph/directed-graph.component';
-import { DirectedGraphService } from './services/directed-graph.service';
 
 const diagramsRoutes: Routes = [{ path: '', component: DiagramsComponent }];
 
 @NgModule({
   declarations: [DiagramsComponent, DirectedGraphComponent],
   imports: [CommonModule, RouterModule.forChild(diagramsRoutes), TranslateModule],
-  providers: [DirectedGraphService],
 })
 export class DiagramsModule {}
