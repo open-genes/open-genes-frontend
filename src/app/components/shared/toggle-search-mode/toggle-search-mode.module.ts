057ac7a9429e3e097e254cc2e9e5ba9e0afff42b
Constantine <kon.rafikov@yandex.ru>
2021-10-24 20:23:55 +0300

    OG-417. Links to Horvath Clock page

--- src/app/components/shared/toggle-search-mode/toggle-search-mode.module.ts
+++ src/app/components/shared/toggle-search-mode/toggle-search-mode.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToggleSearchModeComponent } from './toggle-search-mode.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [ToggleSearchModeComponent],
-  imports: [CommonModule, TranslateModule],
+  imports: [CommonModule, TranslateModule, RouterModule],
   exports: [ToggleSearchModeComponent],
 })
 export class ToggleSearchModeModule {}
