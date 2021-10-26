b831f843082b19b3af83fd3754290eed68cd8c99
Said <nanocoding.said@gmail.com>
2021-10-26 10:29:09 +0500

    OG-405.Finished

--- src/app/core/models/openGenesApi/genes.model.ts
+++ src/app/core/models/openGenesApi/genes.model.ts
@@ -4,6 +4,12 @@ import { Terms } from './gene-ontology.model';
 import { HumanProteinAtlas } from './human-protein-atlas.model';
 import { SelectionCriteria } from './selection-criteria.model';
 import { AssociatedDiseases, AssociatedDiseaseCategories } from './associated-diseases.model';
+import { MethylationCorrelation } from './methylation-correlation.model';
+
+interface TimestampObject {
+  changed: string;
+  created: string;
+}
 
 interface GeneralGeneInfo {
   id: number;
@@ -24,9 +30,9 @@ interface GeneralGeneInfo {
   origin: Origin;
   ncbiId: number;
   uniprot: string;
-  timestamp: string;
+  timestamp?: TimestampObject | string; // TODO: separate models for different API versions
   homologueTaxon: string;
-  methylationCorrelation: 'positive' | 'negative' | '';
+  methylationCorrelation: MethylationCorrelation;
 }
 
 export interface Genes extends GeneralGeneInfo {
