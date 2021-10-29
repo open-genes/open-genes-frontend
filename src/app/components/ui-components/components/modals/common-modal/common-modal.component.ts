0396fe9438265fb2f612ea87df536bb9dd2ec4c5
Said <nanocoding.said@gmail.com>
2021-10-29 10:56:55 +0500

    OG-315. created and implemented ArticlesListModalComponent

---
+++ src/app/components/ui-components/components/modals/common-modal/common-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-common-modal',
+  templateUrl: './common-modal.component.html',
+  styleUrls: ['./common-modal.component.scss']
+})
+export class CommonModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
