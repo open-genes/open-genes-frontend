e397b5c02d1f9cd8e9a985f2979d74e9fd313363
Constantine Rafikov <kon.rafikov@yandex.ru>
2022-01-22 14:54:20 -0300

    Noissue. Code block component

--- src/app/components/ui-components/components/code-block/code-block.component.ts
+++ src/app/components/ui-components/components/code-block/code-block.component.ts
@@ -1,11 +1,8 @@
-import { Component, Input } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-code-block",
-  templateUrl: "./code-block.component.html",
-  styleUrls: ["./code-block.component.scss"],
+  selector: 'app-code-block',
+  templateUrl: './code-block.component.html',
+  styleUrls: ['./code-block.component.scss'],
 })
-export class CodeBlockComponent {
-  @Input()
-  text: string;
-}
+export class CodeBlockComponent {}
