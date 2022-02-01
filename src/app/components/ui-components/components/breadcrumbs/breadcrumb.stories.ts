1b37d643fb6f7858c5ff569d7744535f542dbab0
Said <skamolov@navicons.ru>
2022-02-01 16:33:44 +0500

    OG-325(new) Актуальный Storybook

--- src/app/components/ui-components/components/breadcrumbs/breadcrumb.stories.ts
+++ src/app/components/ui-components/components/breadcrumbs/breadcrumb.stories.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { moduleMetadata, Story } from '@storybook/angular';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
-import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { BreadcrumbService } from './breadcrumb.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 export default {
@@ -13,14 +14,18 @@ export default {
   decorators: [
     moduleMetadata({
       declarations: [BreadcrumbsComponent],
-      imports: [CommonModule, RouterModule, TranslateModule],
+      imports: [CommonModule, RouterTestingModule, TranslateModule, BrowserAnimationsModule],
       providers: [BreadcrumbService],
     }),
   ],
 };
 
-const Template: Story<BreadcrumbsComponent> = () => ({
+const Template: Story<BreadcrumbsComponent> = (args) => ({
+  props: args,
   component: BreadcrumbsComponent,
   template: `<app-breadcrumbs></app-breadcrumbs>`,
 });
 export const Base = Template.bind({});
+Base.args = {
+  breadcrumbs: [{ label: 'home_page_breadcrumb', url: '' }],
+}
