ded6e12482131e55024f8edf8e5bc419cb801895
Constantine <kon.rafikov@yandex.ru>
2022-02-10 01:03:11 -0300

    OG-414. Edits for the purple tables

--- src/app/modules/pipes/pipes.module.ts
+++ src/app/modules/pipes/pipes.module.ts
@@ -10,6 +10,7 @@ import { ZeroPaddingPipe } from './specific/zero-padding.pipe';
 import { PublicationLinksWrapperPipe } from './specific/publication-links-wrapper.pipe';
 import { getResearchStatsById } from './specific/getResearchStatsById.pipe';
 import { AnchorLinksPipe } from './specific/anchor-links.pipe';
+import { TrueFalseNullPipe } from './specific/true-false-null';
 
 const GENERAL_PIPES = [
   SafePipe,
@@ -25,6 +26,7 @@ const SPECIFIC_PIPES = [
   PublicationLinksWrapperPipe,
   AnchorLinksPipe,
   getResearchStatsById,
+  TrueFalseNullPipe,
 ];
 
 const PIPES = [...GENERAL_PIPES, ...SPECIFIC_PIPES];
