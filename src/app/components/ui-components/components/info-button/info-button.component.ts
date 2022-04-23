2d5f54182bf2e3ef7ad88fd8f9f190faf6b9b56b
Constantine <kon.rafikov@yandex.ru>
2022-04-22 23:49:52 -0300

    OG-462. Layout, svg scheme mask, localization

--- src/app/components/ui-components/components/info-button/info-button.component.ts
+++ src/app/components/ui-components/components/info-button/info-button.component.ts
@@ -3,14 +3,12 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-info-button',
   templateUrl: './info-button.component.html',
-  styleUrls: ['./info-button.component.scss']
+  styleUrls: ['./info-button.component.scss'],
 })
 export class InfoButtonComponent implements OnInit {
-  @Input() visible: boolean = false;
+  @Input() visible = false;
   @Output() btnEvent: EventEmitter<Event> = new EventEmitter<Event>();
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
