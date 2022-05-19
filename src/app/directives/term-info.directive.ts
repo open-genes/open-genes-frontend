07f3e4fbbc174cc8c6fae8835d120058c820eb03
Constantine <kon.rafikov@yandex.ru>
2022-05-19 17:19:11 -0300

    OG-834. Fix TermInfoDirective replacing publicationLinksWrap pipe result

--- src/app/directives/term-info.directive.ts
+++ src/app/directives/term-info.directive.ts
@@ -33,7 +33,8 @@ export class TermInfoDirective implements AfterViewInit, OnDestroy {
     if (!this.isApplicable) {
       return;
     }
-    this.content = this.elementRef.nativeElement.innerText;
+    this.content = this.elementRef.nativeElement.innerHTML;
+    console.log("content: ", this.content);
     this.getTermsByLang();
   }
 
