6d39015455d0e373c5256848591cf1f661aacdc7
Constantine <kon.rafikov@yandex.ru>
2022-05-26 01:29:25 -0300

    OG-822. Search modes icons. Gene Ontology icon.

--- src/app/components/shared/toggle-search-mode/toggle-search-mode.module.ts
+++ src/app/components/shared/toggle-search-mode/toggle-search-mode.module.ts
@@ -4,10 +4,11 @@ import { ToggleSearchModeComponent } from './toggle-search-mode.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../modules/vendors/material.module';
+import { IconModule } from "../../ui-components/components/icon/app-icon.module";
 
 @NgModule({
   declarations: [ToggleSearchModeComponent],
-  imports: [CommonModule, TranslateModule, RouterModule, MaterialModule],
+  imports: [CommonModule, TranslateModule, RouterModule, MaterialModule, IconModule],
   exports: [ToggleSearchModeComponent],
 })
 export class ToggleSearchModeModule {}
