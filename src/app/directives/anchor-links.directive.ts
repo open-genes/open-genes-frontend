7aa3797ac1521f6f673c116e8959c61fd8cf073f
Constantine <kon.rafikov@yandex.ru>
2022-08-09 16:46:47 -0300

    OG-772. Fix directive to avoid side effects

--- src/app/directives/anchor-links.directive.ts
+++ src/app/directives/anchor-links.directive.ts
@@ -7,19 +7,20 @@ export class DynamicContentAnchorsDirective implements AfterViewInit {
   constructor(private ngZone: NgZone) {}
   public ngAfterViewInit(): void {
     this.ngZone.runOutsideAngular(() => {
-      console.log('DynamicContentAnchorsDirective');
       setTimeout(() => {
-        Object.values(document.getElementsByTagName('a')).map((el) => {
+        const content = document.getElementById('dynamic-content');
+        Object.values(content.getElementsByTagName('a')).map((el) => {
           el.addEventListener(
             'click',
             function (e) {
-              e.preventDefault();
-              console.log(e);
-              const destinationId = el.getAttribute('href').split('#').pop();
-              if (destinationId?.length !== 0) {
-                const destination = document.getElementById(destinationId);
-                if (destination) {
-                  window.scrollTo(0, destination.offsetTop);
+              if (el.getAttribute('href').includes('#')) {
+                e.preventDefault();
+                const destinationId = el.getAttribute('href').split('#').pop();
+                if (destinationId?.length !== 0) {
+                  const destination = document.getElementById(destinationId);
+                  if (destination) {
+                    window.scrollTo(0, destination.offsetTop);
+                  }
                 }
               }
             },
